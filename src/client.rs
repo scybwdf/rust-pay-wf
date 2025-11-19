56be467773b943c5db97001a39e9f222217ee69c
1054478332@qq.com <1054478332@qq.com>
2025-11-19 15:55:37 +0800

    更新

--- src/client.rs
+++ src/client.rs
@@ -13,30 +13,32 @@ impl Pay {
     pub fn is_config() -> bool {
         CONFIG.get().is_some()
     }
-    pub fn wechat(over_config: WechatConfigOverride) -> crate::wechat::client::WechatClient {
+    pub fn wechat(over_config: Option<WechatConfigOverride>) -> crate::wechat::client::WechatClient {
         let cfg = Self::cfg();
         let wx = cfg.wechat.clone().expect("wechat config missing");
         let mut final_config = (*wx).clone();  // 显式克隆内部数据
 
-        // 应用覆盖配置
-        if let Some(sub_mchid) = over_config.sub_mchid {
-            if !sub_mchid.is_empty() {
-                final_config.sub_mchid = Some(sub_mchid);
+        if let Some(over_config) = over_config {
+            // 应用覆盖配置
+            if let Some(sub_mchid) = over_config.sub_mchid {
+                if !sub_mchid.is_empty() {
+                    final_config.sub_mchid = Some(sub_mchid);
+                }
             }
         }
-
         let final_wx_config = Arc::new(final_config);
         crate::wechat::client::WechatClient::with_mode(final_wx_config, cfg.mode.clone())
     }
-    pub fn alipay(over_config: AlipayConfigOverride) -> crate::alipay::client::AlipayClient {
+    pub fn alipay(over_config: Option<AlipayConfigOverride>) -> crate::alipay::client::AlipayClient {
         let cfg = Self::cfg();
         let ali = cfg.alipay.clone().expect("alipay config missing");
         let mut final_config = (*ali).clone();  // 显式克隆内部数据
-
-        // 应用覆盖配置
-        if let Some(app_auth_token) = over_config.app_auth_token {
-            if !app_auth_token.is_empty() {
-                final_config.app_auth_token = Some(app_auth_token);
+        if let Some(over_config) = over_config {
+            // 应用覆盖配置
+            if let Some(app_auth_token) = over_config.app_auth_token {
+                if !app_auth_token.is_empty() {
+                    final_config.app_auth_token = Some(app_auth_token);
+                }
             }
         }
         let final_ali_config = Arc::new(final_config);
